a96b6349143384ec5a3eac3625fd08354b003689
amr-daoud <amr.f.daoud@gmail.com>
2024-03-12 11:39:14 +0300

    Solving errorInterceptorFn Issue

--- projects/techteec-lib/dialogs-and-templates/src/error/error.interceptor.ts
+++ projects/techteec-lib/dialogs-and-templates/src/error/error.interceptor.ts
@@ -4,27 +4,27 @@ import { ErrorService } from './error.service';
 import { inject } from '@angular/core';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  
+  const errorService = inject(ErrorService);
   return next(req).pipe(
     catchError((error: any) => {
       if(error.status === 0) {
-        inject(ErrorService).open(error.status, 'Please check your connection!');
+        errorService.open(error.status, 'Please check your connection!');
       }
       else if(error.status === 401 || error.status === 403) {
         //this.errorService.open(error.status, 'Your are not authorized to perform this action!');
-        inject(ErrorService).open(error.status, 'You are not authorized to perform this action!');
+        errorService.open(error.status, 'You are not authorized to perform this action!');
       }
       else if(error.status === 500) {
-        inject(ErrorService).open(error.status, 'Something went wrong please report the error to developers!');
+        errorService.open(error.status, 'Something went wrong please report the error to developers!');
       }
       else if(error && error.error && error.error.message) {
         // const m = this.languageService.currentLanguage === 'ar' && error.error.messageArabic ? error.error.messageArabic : error.error.message;
         const m = error.error.message;
-        inject(ErrorService).open(error.status, m);
+        errorService.open(error.status, m);
       } else if(error && error.message) {
-        inject(ErrorService).open(error.status, error.message);
+        errorService.open(error.status, error.message);
       } else {
-        inject(ErrorService).open(error.status, JSON.stringify(error));
+        errorService.open(error.status, JSON.stringify(error));
       }
     throw error;
     })
